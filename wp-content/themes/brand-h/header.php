62cca06f5d7fd85493c843bb30d985da220d339f
Waleed <waleed.ahmad@inter-connect.world>
2020-08-16 11:38:11 +0200

    first setup

--- wp-content/themes/brand-h/header.php
+++ wp-content/themes/brand-h/header.php
@@ -1,45 +1,63 @@
-<?php 
-	
-	/*
+<?php
+
+/*
 		This is the template for the hedaer
 		
 		@package sunsettheme
 	*/
-	
+
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<title><?php bloginfo( 'name' ); wp_title(); ?></title>
-		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<!--Begin tailwind-->
-		<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-		<!--End tailwind-->
-		<?php if( is_singular() && pings_open( get_queried_object() ) ): ?>
-			<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<?php endif; ?>
-		<?php wp_head(); ?>
-		
-		<?php 
-			$custom_css = esc_attr( get_option( 'sunset_css' ) );
-			if( !empty( $custom_css ) ):
-				echo '<style>' . $custom_css . '</style>';
-			endif;
-		?>
-		
-	</head>
+
+<head>
+	<title><?php bloginfo('name');
+			wp_title(); ?></title>
+	<meta name="description" content="<?php bloginfo('description'); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<!--Begin tailwind-->
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+	<!--End tailwind-->
+	<?php if (is_singular() && pings_open(get_queried_object())) : ?>
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<?php endif; ?>
+	<?php wp_head(); ?>
+
+	<?php
+	$custom_css = esc_attr(get_option('sunset_css'));
+	if (!empty($custom_css)) :
+		echo '<style>' . $custom_css . '</style>';
+	endif;
+	?>
+
+</head>
 
 <body <?php body_class(); ?>>
 
-	header
-	
-	
-	
-	
-	
-	
-	
-	
+	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+		<a class="navbar-brand" href="#">Navbar</a>
+		<button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation"></button>
+		<div class="collapse navbar-collapse" id="collapsibleNavId">
+			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+					<div class="dropdown-menu" aria-labelledby="dropdownId">
+						<a class="dropdown-item" href="#">Action 1</a>
+						<a class="dropdown-item" href="#">Action 2</a>
+					</div>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+				<input class="form-control mr-sm-2" type="text" placeholder="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</form>
+		</div>
+	</nav>
