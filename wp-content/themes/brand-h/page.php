0d0e17ad72aa25411ccf752d7200637ba8e0d62c
ryancho <chowin21@gmail.com>
2020-08-17 03:38:37 +0900

    Revert "Revert "Merge branch 'master' of https://github.com/ryancho/wordpress-dev""
    This reverts commit e72463ba96581a414eb41e0eae13576151f56061.

--- wp-content/themes/brand-h/page.php
+++ wp-content/themes/brand-h/page.php
@@ -5,29 +5,29 @@
 */
 
 get_header(); ?>
-	
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			
-			<div class="container">
-				
-				<?php 
-					
-					if( have_posts() ):
-						
-						while( have_posts() ): the_post();
 
-							get_template_part( 'template-parts/content', 'page' );
-						
-						endwhile;
-						
-					endif;
-                
-				?>
-				
-			</div><!-- .container -->
-			
-		</main>
-	</div><!-- #primary -->
-	
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<div class="main">
+
+			<?php
+
+			if (have_posts()) :
+
+				while (have_posts()) : the_post();
+
+					get_template_part('template-parts/content', 'page');
+
+				endwhile;
+
+			endif;
+
+			?>
+
+		</div><!-- .container -->
+
+	</main>
+</div><!-- #primary -->
+
 <?php get_footer(); ?>
