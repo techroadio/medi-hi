e72463ba96581a414eb41e0eae13576151f56061
ryancho <chowin21@gmail.com>
2020-08-17 02:27:09 +0900

    Revert "Merge branch 'master' of https://github.com/ryancho/wordpress-dev"
    This reverts commit 916cb3f75504820af279b1b16116eed250a3bde5, reversing
    changes made to 7ee88dce930f8dc9730be82a2328b45fa423d021.

--- wp-content/themes/brand-h/header.php
+++ wp-content/themes/brand-h/header.php
@@ -19,8 +19,6 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<!--Begin tailwind-->
 	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
-	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 	<!--End tailwind-->
 	<?php if (is_singular() && pings_open(get_queried_object())) : ?>
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
@@ -34,60 +32,8 @@
 	endif;
 	?>
 
-	<style>
-		.medi-cover {
-			background-image: url("<?php bloginfo('template_directory') ?>/img/main_cover.jpg");
-			background-repeat: no-repeat;
-			background-size: cover;
-			min-height: 520px;
-		}
-
-		.endCont {
-			width: 100%;
-			height: 500px;
-			background-image: url("<?php bloginfo('template_url') ?>/img/bg-end.jpg");
-			background-repeat: no-repeat;
-			background-size: cover;
-		}
-
-		.swiper-container {
-			cursor: pointer;
-			min-height: 340px;
-		}
-
-		.swiper-scrollbar {
-			bottom: 15px !important;
-		}
-
-		.swiper-scrollbar-drag {
-			background: #ffffff;
-		}
-	</style>
-
 </head>
 
 <body <?php body_class(); ?>>
 
-	<!-- Header (start) -->
-	<header class="flex h-30 bg-white">
-		<!-- Logo (start) -->
-		<div class="w-3/4">
-			<img class="w-3/4 mx-auto" src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="">
-		</div>
-		<!-- Logo (end) -->
-		<!-- Nav Menu (start) -->
-		<div class="w-1/4">
-			<nav class="flex items-center justify-between flex-wrap p-6">
-				<div class="block">
-					<button class="flex text-4xl items-center px-5 py-3 border rounded text-gray-700 border-gray-700">
-						<svg class="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-							<title>Menu</title>
-							<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-						</svg>
-					</button>
-				</div>
-			</nav>
-		</div>
-		<!-- Nav Menu (end) -->
-	</header>
-	<!-- Header (end) -->
+	headers
