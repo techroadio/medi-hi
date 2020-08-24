d2b2f49cfa303f1ea85e34b6253f391f3158722a
ryancho <chowin21@gmail.com>
2020-08-24 19:23:24 +0900

    add:affiliate

--- wp-content/themes/brand-h/inc/templates/affiliate.php
+++ wp-content/themes/brand-h/inc/templates/affiliate.php
@@ -33,4 +33,35 @@
       <img class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/14-logo.png" >
     </div>
   </div>
+  
+  <!--Hidden-popup (start)  -->
+
+
+  
+
+<!-- <div class="" style="background-color: rgba(0, 0, 0, 0.3);z-index:9999"> -->
+<div class="" >
+  <div class="py-5 px-5 bg-wihte" style="z-index:1">  
+    <div alt="hospital image"
+              class="bg-cover content-partners"
+              style="background-image: url(<?php bloginfo('template_directory') ?>/img/partners/01.jpg);">
+    </div>
+    <div class="flex justify-center">
+      <div class="" style="margin-top:-60px;border-radius:50%;border: 1px solid rgba(0,0,0,.1);z-index:1">
+          <img  src="https://himedi.s3.ap-northeast-2.amazonaws.com/hospitals/logos/17.png" width="92" height="92" alt="hospital logo" class="bg-white rounded-full ">
+      </div>
+    </div>
+    <div class="font-size-22 text-b80 text-center margin-28" style="margin-top: 10px;"> CHA Gangnam
+          Medical Center
+    </div>
+    <div class="font-size-16 text-b50 text-center margin-28 h-line-1-4 font-light keep-direction"
+          style="margin: 8px 28px 14px;">#IVF #Infertilityspecialtyhospital #Infertilitycounseling
+    </div>
+  </div>
+</div>
+
+    
+   
+  <!--Hidden-popup (end)  -->
+
 <!-- Section 3-3 (end) -->
